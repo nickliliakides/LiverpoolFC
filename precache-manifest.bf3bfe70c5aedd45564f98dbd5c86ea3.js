53691422d3aeb729d292b43459abf98d4e886eb0
Your Name <nickliliakides@gmail.com>
2019-07-25 23:40:54 +0100

    Updates

---
+++ precache-manifest.bf3bfe70c5aedd45564f98dbd5c86ea3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc34f9a5367c0242345a09235bbdd67",
+    "url": "/LiverpoolFC/index.html"
+  },
+  {
+    "revision": "9362ebc7e92651288f91",
+    "url": "/LiverpoolFC/static/css/main.59101a6c.chunk.css"
+  },
+  {
+    "revision": "86ebe3aba29dad0bba38",
+    "url": "/LiverpoolFC/static/js/2.8fe2a5d9.chunk.js"
+  },
+  {
+    "revision": "9362ebc7e92651288f91",
+    "url": "/LiverpoolFC/static/js/main.80b70c38.chunk.js"
+  },
+  {
+    "revision": "a700eadee11d7c49a65d",
+    "url": "/LiverpoolFC/static/js/runtime~main.195a0d7d.js"
+  },
+  {
+    "revision": "98ef62ba7a480b5bc3a2cd706a6a1ea4",
+    "url": "/LiverpoolFC/static/media/Liverpool-jersey.98ef62ba.jpg"
+  },
+  {
+    "revision": "8980949d876cc3583754ffe2e7ccc129",
+    "url": "/LiverpoolFC/static/media/SalahCover.8980949d.png"
+  },
+  {
+    "revision": "49fb318ee81af2c44c11b1f8000f6882",
+    "url": "/LiverpoolFC/static/media/liverpool.49fb318e.png"
+  },
+  {
+    "revision": "14840165b1757521c98020093ba865ce",
+    "url": "/LiverpoolFC/static/media/virgil_van_dijk.14840165.png"
+  }
+]);
